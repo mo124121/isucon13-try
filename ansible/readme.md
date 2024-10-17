26241c39db3ba868cf238194d81b856e969e9b42
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-10-17 14:53:32 +0000

    add pdns modify

--- ansible/readme.md
+++ ansible/readme.md
@@ -8,6 +8,8 @@ https://github.com/rinchsan/isucon13-final
 
 ansible-playbook  -i inventory.yaml -u ubuntu --private-key ~/.ssh/codespace.pem [実行したいplaybook].yaml
 
+ansible-playbook  -i inventory.yaml -u ubuntu --private-key ~/.ssh/codespace.pem init_server.yaml
+
 ## 編集が必要な個所
 
 inventory.yamlにデプロイ先のホストの指定がある
--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -2,3 +2,16 @@ use isupipe;
 ALTER TABLE `livestream_tags` ADD INDEX livestream_id_idx(livestream_id);
 ALTER TABLE `icons` ADD INDEX user_id_idx(user_id);
 ALTER TABLE `themes` ADD INDEX user_id_idx(user_id);
+
+ALTER TABLE `livestreams` ADD INDEX user_id_idx(user_id);
+ALTER TABLE `livecomments` ADD INDEX user_id_idx(user_id);
+ALTER TABLE `livecomments` ADD INDEX livestream_id_idx(livestream_id);
+
+ALTER TABLE `reactions` ADD INDEX user_id_idx(user_id);
+ALTER TABLE `reactions` ADD INDEX livestream_id_idx(livestream_id);
+
+ALTER TABLE `ng_words` ADD INDEX user_id_idx(user_id);
+ALTER TABLE `ng_words` ADD INDEX livestream_id_idx(livestream_id);
+
+use isudns;
+ALTER TABLE `records` ADD INDEX name_idx(name);
