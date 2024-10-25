3306154e633f2f7b2db633ffe842291cb21c32c7
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-10-25 12:52:36 +0000

    fix init server

--- webapp/pdns/initdb.d/00_create_database.sql
+++ webapp/pdns/initdb.d/00_create_database.sql
@@ -5,6 +5,11 @@ DROP USER IF EXISTS `isudns`@`localhost`;
 CREATE USER 'isudns'@'localhost' IDENTIFIED BY 'isudns';
 GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'localhost';
 
-DROP USER IF EXISTS `isudns`@`pipe.u.isucon.local`;
-CREATE USER 'isudns'@'pipe.u.isucon.local' IDENTIFIED BY 'isudns';
-GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'pipe.u.isucon.local';
+DROP USER IF EXISTS `isudns`@`54.199.66.128`;
+CREATE USER 'isudns'@'54.199.66.128' IDENTIFIED BY 'isudns';
+GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'54.199.66.128';
+
+DROP USER IF EXISTS `isudns`@`18.182.12.217`;
+CREATE USER 'isudns'@'18.182.12.217' IDENTIFIED BY 'isudns';
+GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'18.182.12.217';
+
